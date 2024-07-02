b4d0dfe87e7c3386146346b3a5c462103571faca
Matthew Benedicta <matthewbenedicta2007@gmail.com>
2024-07-02 13:41:23 +0100

    fix structure of bonus section and fix first bonus logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,11 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Cards from "../components/Card";
+import img1 from "../asset/costco41.png";
+import img2 from "../asset/costco39.png";
+import img3 from "../asset/costco48.svg";
+import img4 from "../asset/costco49.svg";
+import "../css/home.css";
 
 const Home = () => {
   return (
@@ -13,7 +18,48 @@ const Home = () => {
         <Cards />
       </div>
 
-      <div className="bonus"></div>
+      <div className="bonusContainer">
+        <div className="bonusTextContainer">
+          <div className="bonusLogoSection">
+            <div className="bonusLogo1">
+              <img src={img1} alt="" />
+              <img src={img2} alt="" />
+            </div>
+            <div className="bonusLogo2">
+              <h1>Benefit from Buying More</h1>
+              <div>
+                <img src={img3} alt="" />
+                <img src={img3} alt="" />
+              </div>
+            </div>
+          </div>
+          <div>
+            <p>
+              Costco Direct Benefit from Buying More Combine with other
+              promotions for additional savings!
+            </p>
+            <p>Delivery in 3-5 Days in Most Areas*</p>
+            <h3>
+              {" "}
+              Participating items are marked{" "}
+              <span>
+                <img src={img4} alt="" />
+              </span>
+            </h3>
+          </div>
+        </div>
+
+        <div className="bonusCardContainer">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <p>
+          ONLINE-ONLY | LIMIT 2 REDEMPTIONS PER MEMBERSHIP | *SEE ITEM PAGE FOR
+          DETAILS
+        </p>
+      </div>
       <Footer />
     </>
   );
