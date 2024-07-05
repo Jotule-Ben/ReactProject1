b7fa1fd7084bc43c1515abf160ce1e787ade6182
Matthew Benedicta <matthewbenedicta2007@gmail.com>
2024-07-05 16:55:20 +0100

    fix shop banner section

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import banner from "../asset/BbyHeroImg.png";
 
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <div className="shop">
+      <p>
+        <Link to="/">Home</Link> / Baby
+      </p>
+
+      <h3>Baby</h3>
+
+      <div className="bbyHero">
+        <img src={banner} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
