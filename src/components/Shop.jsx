b1d25f9c9f4810d4eb60c25e79b2d13d7b4f4898
Matthew Benedicta <matthewbenedicta2007@gmail.com>
2024-07-05 23:46:32 +0100

    fix shop card section

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -15,6 +15,10 @@ import mushie from "../asset/mushie.png";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { IoGridSharp, IoMenuSharp } from "react-icons/io5";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { FaStar } from "react-icons/fa6";
+import { TfiAngleRight } from "react-icons/tfi";
 
 const Shop = () => {
   return (
@@ -94,6 +98,304 @@ const Shop = () => {
             </p>
 
             <h6>Showing 1-24 of 43</h6>
+          </div>
+        </div>
+
+        <div className="cardContainer">
+          <div className="cardsec">
+            <Card className="cardContent">
+              <Card.Img variant="top" src={card6} />
+              <Card.Body>
+                <Card.Title>$21.99</Card.Title>
+                <Link>
+                  Kirkland signature baby wipes fragrance free, 900-count
+                </Link>
+                <br />
+                <Link>
+                  <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                  (10140)
+                </Link>
+                <Card.Text>Delivery Available</Card.Text>
+                <p>
+                  {" "}
+                  <input type="checkbox" />
+                  <span>Compare Product</span>
+                </p>
+                <div className="cardFooter">
+                  <span>-</span> <span>1</span> <span>+</span>
+                  <Button variant="primary">Add</Button>
+                </div>
+              </Card.Body>
+            </Card>
+
+            <Card className="cardContent">
+              <Card.Img variant="top" src={card7} />
+              <Card.Body>
+                <Card.Title>$21.99</Card.Title>
+                <Link>Kirkland signature Diapers Sizes 3-6</Link>
+                <br />
+                <Link>
+                  <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                  (19834)
+                </Link>
+                <Card.Text>Delivery Available</Card.Text>
+                <p>
+                  {" "}
+                  <input type="checkbox" />
+                  <span>Compare Product</span>
+                </p>
+                <div className="cardFooter">
+                  <Button variant="primary">Select Options</Button>
+                </div>
+              </Card.Body>
+            </Card>
+
+            <Card className="cardContent">
+              <Card.Img variant="top" src={card8} />
+              <Card.Body>
+                <Card.Title>$21.99</Card.Title>
+                <Link>
+                  Kirkland Signature ProCare with Dual HMO's, Non-GMO Infant
+                  Formula 42 oz, 2-pack
+                </Link>
+                <br />
+                <Link>
+                  <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                  (2748)
+                </Link>
+                <Card.Text>Delivery Available</Card.Text>
+                <p>
+                  {" "}
+                  <input type="checkbox" />
+                  <span>Compare Product</span>
+                </p>
+                <div className="cardFooter">
+                  <Button variant="primary">Sign In for Details</Button>
+                </div>
+              </Card.Body>
+            </Card>
+
+            <Card className="cardContent">
+              <Card.Img variant="top" src={card9} />
+              <Card.Body>
+                <Card.Title>$21.99</Card.Title>
+                <Link>Kirkland signature Diapers Sizes 1-2</Link>
+                <br />
+                <Link>
+                  <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                  (4898)
+                </Link>
+                <Card.Text>Delivery Available</Card.Text>
+                <p>
+                  {" "}
+                  <input type="checkbox" />
+                  <span>Compare Product</span>
+                </p>
+                <div className="cardFooter">
+                  <Button variant="primary">Select Options</Button>
+                </div>
+              </Card.Body>
+            </Card>
+
+            <Card className="cardContent">
+              <Card.Img variant="top" src={card6} />
+              <Card.Body>
+                <Card.Title>$21.99</Card.Title>
+                <Link>
+                  Kirkland signature baby wipes fragrance free, 900-count
+                </Link>
+                <br />
+                <Link>
+                  <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                  (10140)
+                </Link>
+                <Card.Text>Delivery Available</Card.Text>
+                <p>
+                  {" "}
+                  <input type="checkbox" />
+                  <span>Compare Product</span>
+                </p>
+                <div className="cardFooter">
+                  <span>-</span> <span>1</span> <span>+</span>
+                  <Button variant="primary">Add</Button>
+                </div>
+              </Card.Body>
+            </Card>
+
+            <Card className="cardContent">
+              <Card.Img variant="top" src={card7} />
+              <Card.Body>
+                <Card.Title>$21.99</Card.Title>
+                <Link>Kirkland signature Diapers Sizes 3-6</Link>
+                <br />
+                <Link>
+                  <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                  (19834)
+                </Link>
+                <Card.Text>Delivery Available</Card.Text>
+                <p>
+                  {" "}
+                  <input type="checkbox" />
+                  <span>Compare Product</span>
+                </p>
+                <div className="cardFooter">
+                  <Button variant="primary">Select Options</Button>
+                </div>
+              </Card.Body>
+            </Card>
+
+            <Card className="cardContent">
+              <Card.Img variant="top" src={card8} />
+              <Card.Body>
+                <Card.Title>$21.99</Card.Title>
+                <Link>
+                  Kirkland Signature ProCare with Dual HMO's, Non-GMO Infant
+                  Formula 42 oz, 2-pack
+                </Link>
+                <br />
+                <Link>
+                  <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                  (2748)
+                </Link>
+                <Card.Text>Delivery Available</Card.Text>
+                <p>
+                  {" "}
+                  <input type="checkbox" />
+                  <span>Compare Product</span>
+                </p>
+                <div className="cardFooter">
+                  <Button variant="primary">Sign In for Details</Button>
+                </div>
+              </Card.Body>
+            </Card>
+
+            <Card className="cardContent">
+              <Card.Img variant="top" src={card9} />
+              <Card.Body>
+                <Card.Title>$21.99</Card.Title>
+                <Link>Kirkland signature Diapers Sizes 1-2</Link>
+                <br />
+                <Link>
+                  <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                  (4898)
+                </Link>
+                <Card.Text>Delivery Available</Card.Text>
+                <p>
+                  {" "}
+                  <input type="checkbox" />
+                  <span>Compare Product</span>
+                </p>
+                <div className="cardFooter">
+                  <Button variant="primary">Select Options</Button>
+                </div>
+              </Card.Body>
+            </Card>
+
+            <Card className="cardContent">
+              <Card.Img variant="top" src={card6} />
+              <Card.Body>
+                <Card.Title>$21.99</Card.Title>
+                <Link>
+                  Kirkland signature baby wipes fragrance free, 900-count
+                </Link>
+                <br />
+                <Link>
+                  <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                  (10140)
+                </Link>
+                <Card.Text>Delivery Available</Card.Text>
+                <p>
+                  {" "}
+                  <input type="checkbox" />
+                  <span>Compare Product</span>
+                </p>
+                <div className="cardFooter">
+                  <span>-</span> <span>1</span> <span>+</span>
+                  <Button variant="primary">Add</Button>
+                </div>
+              </Card.Body>
+            </Card>
+
+            <Card className="cardContent">
+              <Card.Img variant="top" src={card7} />
+              <Card.Body>
+                <Card.Title>$21.99</Card.Title>
+                <Link>Kirkland signature Diapers Sizes 3-6</Link>
+                <br />
+                <Link>
+                  <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                  (19834)
+                </Link>
+                <Card.Text>Delivery Available</Card.Text>
+                <p>
+                  {" "}
+                  <input type="checkbox" />
+                  <span>Compare Product</span>
+                </p>
+                <div className="cardFooter">
+                  <Button variant="primary">Select Options</Button>
+                </div>
+              </Card.Body>
+            </Card>
+
+            <Card className="cardContent">
+              <Card.Img variant="top" src={card8} />
+              <Card.Body>
+                <Card.Title>$21.99</Card.Title>
+                <Link>
+                  Kirkland Signature ProCare with Dual HMO's, Non-GMO Infant
+                  Formula 42 oz, 2-pack
+                </Link>
+                <br />
+                <Link>
+                  <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                  (2748)
+                </Link>
+                <Card.Text>Delivery Available</Card.Text>
+                <p>
+                  {" "}
+                  <input type="checkbox" />
+                  <span>Compare Product</span>
+                </p>
+                <div className="cardFooter">
+                  <Button variant="primary">Sign In for Details</Button>
+                </div>
+              </Card.Body>
+            </Card>
+
+            <Card className="cardContent">
+              <Card.Img variant="top" src={card9} />
+              <Card.Body>
+                <Card.Title>$21.99</Card.Title>
+                <Link>Kirkland signature Diapers Sizes 1-2</Link>
+                <br />
+                <Link>
+                  <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar />
+                  (4898)
+                </Link>
+                <Card.Text>Delivery Available</Card.Text>
+                <p>
+                  {" "}
+                  <input type="checkbox" />
+                  <span>Compare Product</span>
+                </p>
+                <div className="cardFooter">
+                  <Button variant="primary">Select Options</Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="cardSecFooter">
+            <div className="cardFooterPagination">
+              <p>1</p>
+              <p>2</p>
+              <p>
+                <TfiAngleRight />
+              </p>
+            </div>
+            <div>
+              <Link>Back To Top</Link>
+            </div>
           </div>
         </div>
       </div>
