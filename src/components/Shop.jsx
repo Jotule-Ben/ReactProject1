1694617fcc0232b1bb6be2163f17b89324494a6a
Matthew Benedicta <matthewbenedicta2007@gmail.com>
2024-07-05 21:53:25 +0100

    fix shop sort section

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -11,8 +11,10 @@ import card6 from "../asset/bbySecCard1.png";
 import card7 from "../asset/bbySecCard2.png";
 import card8 from "../asset/bbySecCard3.png";
 import card9 from "../asset/bbySecCard4.png";
+import mushie from "../asset/mushie.png";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { IoGridSharp, IoMenuSharp } from "react-icons/io5";
 
 const Shop = () => {
   return (
@@ -52,6 +54,46 @@ const Shop = () => {
               <img src={card5} alt="" />
               <p>Clothing for babies</p>
             </div>
+          </div>
+        </div>
+
+        <div>
+          <h5>Shop costco next - buy direct from select costco suppliers</h5>
+          <img src={mushie} alt="" />
+          <p>mushie</p>
+        </div>
+
+        <div className="sort">
+          <div>
+            <div>
+              <span>Sort by: </span>{" "}
+              <select>
+                <option>Best Match</option>
+                <option>Price (High to Low)</option>
+                <option>Price (Low to High)</option>
+                <option>Ratings (High to Low)</option>
+                <option>Newest</option>
+                <option>Most Viewed</option>
+              </select>
+            </div>
+            <div className="sortBtn">
+              <button>
+                Delivery <span>&times;</span>
+              </button>
+              <button>
+                Show Out of Stock Items <span>&times;</span>
+              </button>
+            </div>
+          </div>
+          <div className="toggle">
+            <p>
+              <IoGridSharp className="IoGridSharp" />
+            </p>
+            <p>
+              <IoMenuSharp className="IoMenuSharp" />
+            </p>
+
+            <h6>Showing 1-24 of 43</h6>
           </div>
         </div>
       </div>
