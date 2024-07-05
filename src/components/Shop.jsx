5dec1d3b944c2585c95ac8907f1bb2c39631812b
Matthew Benedicta <matthewbenedicta2007@gmail.com>
2024-07-06 00:09:32 +0100

    fix shop last section

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -398,6 +398,82 @@ const Shop = () => {
             </div>
           </div>
         </div>
+
+        <div className="moreAbout">
+          <h3>Help Your Baby Grow Strong & Healthy</h3>
+          <p>
+            Good nutrition is essential to your baby’s health and well-being,
+            which is why Costco carries only the best
+            <Link> baby formula & food</Link> for your little one. We offer a
+            variety of infant formulas to suit various dietary needs, all from
+            leading brands like Similac, Enfamil, and Pediasure. You’ll find
+            infant formulas that are organic, gluten-free, and kosher, and many
+            that are formulated for sensitive stomachs. These high-quality baby
+            formulas are enriched with vitamins and nutrients to support the
+            immune system, as well as help with proper brain and eye
+            development. Costco also offers healthy foods and snacks like
+            easy-serve food pouches filled with nutritious fruits and
+            vegetables, yummy organic snack bars, and vitamin-packed protein
+            shakes.
+          </p>
+        </div>
+
+        <div className="moreAbout">
+          <h3>Keep Your Baby Clean & Dry</h3>
+          <p>
+            Raising a family can be expensive, and economy-sized baby items such
+            as
+            <Link> baby formula & food</Link> diapers, wipes & training pants,
+            are a budget-friendly solution to saving you money. At Costco,
+            you’ll find all your favorite baby stuff at exclusive Costco member
+            prices. We carry baby wipes in standard and travel packs, diapers in
+            every size, and colorful training pants for toddlers. You can also
+            stock up on other baby essentials like wipe warmers and Diaper Genie
+            refills. Choose from trusted brands like Huggies, Pull-Ups, and
+            Pampers. And all the money you save, will make it easier to keep up
+            with your growing baby’s needs. So, whether it’s buying beautiful
+            new <Link> nursery furniture</Link>or getting the latest and
+            greatest kid’s toys, you’ll appreciate all the ways that Costco
+            makes your life easier.
+          </p>
+        </div>
+
+        <div className="moreAbout">
+          <h3>Shop Premium Baby Products for Your Car & Home</h3>
+          <p>
+            Costco has a great selection of baby gear for every stage of
+            childhood, from baby carriages to booster seats. You’ll find infant
+            car seats and convertible car seat systems with the latest safety
+            and comfort features, including sturdy steel-reinforced frames,
+            multiple recline positions, plush padding, and 5-point harness
+            systems. We have air purifiers to keep your child’s room free from
+            germs and allergens, as well as comforting sound machines that will
+            soothe your baby to sleep. And for strollers, we carry lightweight
+            jogging strollers with high-impact wheels, roomy double strollers,
+            and convenient car seat strollers for easy transfers in and out of
+            the car.
+          </p>
+        </div>
+
+        <div className="moreAbout">
+          <h3>Monitor Your Baby’s Health & Safety</h3>
+          <p>
+            As a parent, you want to do everything possible to keep your child
+            safe and healthy. That’s why Costco carries high-quality
+            <Link> baby care & safety</Link> items for both babies and toddlers.
+            We offer the best baby monitor cameras and baby monitoring systems
+            on the market, with innovative features like high-resolution HD
+            video, night lights, calming sleep sounds, and 2-way audio that lets
+            you communicate with your child without even entering their room.
+            Many new systems come with WIFI baby monitors and baby monitor apps
+            that allow you to use your smartphone to conveniently check in on
+            your child from almost anywhere in your home. We even have advanced
+            monitoring systems that continuously track your baby’s oxygen level
+            and heart rate, and immediately alert you to any sudden changes.
+            Being able to quickly and easily check your baby’s vitals will give
+            you valuable peace of mind, so you can both sleep better.
+          </p>
+        </div>
       </div>
       <Footer />
     </>
