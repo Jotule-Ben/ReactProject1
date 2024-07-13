b678ac0326a50274160b3f097c252c28682f4ee9
Matthew Benedicta <matthewbenedicta2007@gmail.com>
2024-07-13 15:18:13 +0100

    handle update password validation

--- src/components/UpdateMerchantPassword.jsx
+++ src/components/UpdateMerchantPassword.jsx
@@ -1,32 +1,42 @@
-import React, { useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../css/signup.css";
-import axios from "axios";
 
 const UpdateMerchantPassword = () => {
-  const password = useRef();
-  const newPassword = useRef();
+  const initalValues = { oldPassword: "", newPassword: "" };
+  const [formValues, setFormValues] = useState(initalValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
-  const handleSubmit = () => {
-    const passwordValue = password.current.value;
-    const newPasswordValue = newPassword.current.value;
-    axios
-      .post(
-        `http://ecommerce.reworkstaging.name.ng/v2/merchants/:merchant_id/change-passwd`,
-        {
-          old_password: passwordValue,
-          new_password: newPasswordValue,
-        }
-      )
-      .then(
-        (response) => {
-          console.log(response, response.id);
-          alert("Login Successful!", response.id);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+    console.log(formValues);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+
+  const validate = (values) => {
+    const errors = {};
+    if (!values.oldPassword) {
+      errors.oldPassword = "Old password is required";
+    }
+
+    if (!values.newPassword) {
+      errors.newPassword = "New password is required";
+    }
+    return errors;
   };
 
   return (
@@ -34,13 +44,27 @@ const UpdateMerchantPassword = () => {
       <div className="signUp">
         <div className="signInForm">
           <form>
-            <input type="password" placeholder="old password" ref={password} />
+            <input
+              type="password"
+              name="oldPassword"
+              placeholder="old password"
+              value={formValues.oldPassword}
+              onChange={handleChange}
+            />
+
+            <p className="error">{formErrors.oldPassword}</p>
+            {/* {validate.oldPassword && <span>{validate.oldPassword}</span>} */}
             <br />
             <input
               type="password"
+              name="newPassword"
               placeholder="new password"
-              ref={newPassword}
+              value={formValues.newPassword}
+              onChange={handleChange}
             />
+
+            <p className="error">{formErrors.newPassword}</p>
+            {/* {validate.newPassword && <span>{validate.newPassword}</span>} */}
             <br />
             <button onClick={handleSubmit}>
               <Link to="">Change Password</Link>
